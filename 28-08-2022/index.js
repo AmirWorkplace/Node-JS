50f85ac5c9c8d9baeda15a35925565e020eb34cf
AmirWorkplace <amirralli300400@gmail.com>
2022-09-02 12:24:43 +0600

    Okey

---
+++ 28-08-2022/index.js
@@ -0,0 +1,44 @@
+console.log('');
+// The Wrong System
+/* function products(productWeight, productPrice) {
+  const totalPrice = productWeight * productPrice;
+  return totalPrice;
+}
+const productTotal = products(100, 500);
+console.log(`The price: ${productTotal} Tk!`);
+ */
+// The Reight System
+/* const Product = (price, weight) =>{
+  let totalprice = (price * weight) / 1000;
+  return `The price is: ${totalprice} Tk!`;
+}
+console.log(Product(500, 800));
+console.log(Product(100, 152));
+console.log(Product(235, 685));
+console.log(Product(134, 528));
+console.log("");
+console.log(`Liton Vai!`); */
+/* 
+console.log("Array Starts:");
+
+let array = [
+  'January', 'February', 'Merch', 'April'
+];
+array.push('June')
+array.shift();
+array.pop();
+array.unshift('April');
+
+let array2 = [
+  'Liton', 'Sabbir', 'Amir'
+];
+
+console.log();
+console.log(array);
+ */
+
+
+
+
+
+console.log('');
